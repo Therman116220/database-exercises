293e090dc1d0f3a6dd21c07e9142b2bd1c131f6d
Therman116220 <thermanhaynes@gmail.com>
2022-03-18 11:06:09 -0500

    update

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,4 @@ SELECT name FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT names FROM albums WHERE sales < 20000000;  /*?*/

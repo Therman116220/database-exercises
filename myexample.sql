acb4e76d72684f3cf8ec92cd4fb2cab3376f7a89
Therman116220 <thermanhaynes@gmail.com>
2022-03-18 10:07:58 -0500

    update

--- myexample.sql
+++ myexample.sql
@@ -1,1 +1,73 @@
-USE myexample.sql;
+
+
+
+
+
+--USE codeup_test_db;   //  Changes db
+--Describe albums
+/*-- GET STARTED
+mysql.server status;
+mysql.server start;
+mysql.server stop;
+
+-- -U = USERNAME      -P = PASSWORD (codeup)
+mysql -u root -p;
+-- PASSWORD WON'T SHOW WHEN TYPING IN
+
+
+
+-- Note that all text commands must be first on line and end with ';'
+\h
+-- HELP PAGE WITH COMMANDS LISTED
+\q
+-- QUIT
+\?
+-- Synonym for `help'.
+\c
+-- Clear the current input statement.
+\r
+-- Reconnect to the server. Optional arguments are db and host.
+\d
+-- Set statement delimiter.
+\e
+-- Edit command with $EDITOR.
+\G
+-- Send command to mysql server, display result vertically.
+\q
+-- Exit mysql. Same as quit.
+\g
+-- Send command to mysql server.
+\h
+-- Display this help.
+\n
+-- Disable pager, print to stdout.
+\t
+-- Don't write into outfile.
+\P
+-- Set PAGER [to_pager]. Print the query results via PAGER.
+\p
+-- Print current command.
+\R
+-- Change your mysql prompt.
+\q
+-- Quit mysql.
+\#
+-- Rebuild completion hash.
+\.
+-- Execute an SQL script file. Takes a file name as an argument.
+\s
+-- Get status information from the server.
+\!
+-- Execute a system shell command.
+\T
+-- Set outfile [to_outfile]. Append everything into given outfile.
+\u
+-- Use another database. Takes database name as argument.
+\C
+-- Switch to another charset. Might be needed for processing binlog with multi-byte charsets.
+\W
+-- Show warnings after every statement.
+\w
+-- Don't show warnings after every statement.
+\x
+-- Clean session context.*/

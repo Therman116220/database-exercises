93f0e57c48bc5cc743410470615399f7d5cb68c5
Therman116220 <thermanhaynes@gmail.com>
2022-03-22 19:37:26 -0500

    update

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -5,6 +5,9 @@ USE employees;
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees LIMIT 10;
 
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, CONCAT(birth_date) AS DOB
+FROM employees LIMIT 10;
+
 
 
 
